01add98e9dfd57132391f4a06a67104cc0088313
yechun <yechun.yc@alibaba-inc.com>
2025-04-02 18:00:04 +0800

    feat(fuzz): add openkruise project files for fuzzinging

--- projects/openkruise/build.sh
+++ projects/openkruise/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eu
+#/bin/bash -eu
 # Copyright 2021 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,32 +15,4 @@
 #
 ################################################################################
 
-set -o nounset
-set -o pipefail
-set -o errexit
-set -x
-
-go clean --modcache
-go mod tidy
-go mod vendor
-
-rm -r $SRC/kruise/vendor
-go get github.com/AdamKorcz/go-118-fuzz-build/testing
-
-
-compile_native_go_fuzzer $SRC/kruise/pkg/controller/workloadspread FuzzPatchFavoriteSubsetMetadataToPod fuzz_patch_favorite_subset_metadata_to_pod
-compile_native_go_fuzzer $SRC/kruise/pkg/controller/workloadspread FuzzPodPreferredScore fuzz_pod_preferred_score
-
-compile_native_go_fuzzer $SRC/kruise/pkg/util/workloadspread FuzzInjectWorkloadSpreadIntoPod fuzz_inject_workloadspread_into_pod
-compile_native_go_fuzzer $SRC/kruise/pkg/util/workloadspread FuzzNestedField fuzz_nested_field
-compile_native_go_fuzzer $SRC/kruise/pkg/util/workloadspread FuzzIsPodSelected fuzz_is_pod_selected
-compile_native_go_fuzzer $SRC/kruise/pkg/util/workloadspread FuzzHasPercentSubset fuzz_has_percent_subset
-
-compile_native_go_fuzzer $SRC/kruise/pkg/webhook/workloadspread/validating FuzzValidateWorkloadSpreadSpec fuzz_validate_workloadspread_spec
-compile_native_go_fuzzer $SRC/kruise/pkg/webhook/workloadspread/validating FuzzValidateWorkloadSpreadConflict fuzz_validate_workloadspread_conflict
-compile_native_go_fuzzer $SRC/kruise/pkg/webhook/workloadspread/validating FuzzValidateWorkloadSpreadTargetRefUpdate fuzz_validate_workloadspread_target_ref_update
-
-compile_native_go_fuzzer $SRC/kruise/pkg/controller/uniteddeployment FuzzParseSubsetReplicas fuzz_parse_subset_replicas
-compile_native_go_fuzzer $SRC/kruise/pkg/controller/uniteddeployment FuzzApplySubsetTemplate fuzz_apply_subset_template
-compile_native_go_fuzzer $SRC/kruise/pkg/controller/uniteddeployment FuzzReplicaAllocator fuzz_replica_allocator
-compile_native_go_fuzzer $SRC/kruise/pkg/webhook/uniteddeployment/validating FuzzValidateUnitedDeploymentSpec fuzz_validate_uniteddeployment_spec
+$SRC/containerd/pkg/util/fuzz/oss_fuzz_build.sh
